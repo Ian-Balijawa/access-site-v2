96e79962367af7b55e073d466ecae1ca0d42dd4d
ian balijawa <ianbalijawa16@gmail.com>
2021-10-12 13:42:12 +0300

    initial commit

---
+++ src/helpers/ReactModalAdapter.js
@@ -0,0 +1,14 @@
+import Modal from 'react-modal';
+
+/* This just wraps react-modal to allow styling the modal overlay, you shouldn't have the need to change this at all */
+export default ({ className, ...props }) => {
+	const contentClassName = `${className}__content`;
+	const overlayClassName = `${className}__overlay`;
+	return (
+		<Modal
+			className={contentClassName}
+			overlayClassName={overlayClassName}
+			{...props}
+		/>
+	);
+};
---
+++ src/helpers/ResponsiveVideoEmbed.js
@@ -0,0 +1,29 @@
+/* A simple iframe wrapper that makes 16:9 responsive embed */
+
+export default ({ url, background = 'black', className = 'video' }) => {
+	return (
+		<div
+			className={className}
+			style={{
+				position: 'relative',
+				background: background,
+				paddingBottom: '56.25%' /* 16:9 */,
+				paddingTop: 25,
+				height: 0,
+			}}
+		>
+			<iframe
+				title='Embeded Video'
+				style={{
+					position: 'absolute',
+					top: 0,
+					left: 0,
+					width: '100%',
+					height: '100%',
+				}}
+				src={url}
+				frameBorder='0'
+			/>
+		</div>
+	);
+};
